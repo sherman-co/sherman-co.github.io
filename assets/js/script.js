87a7d3b9bcefeee8b8fa400eec02a2dab507af2a
sherman-co <amir.maleki2070@gmail.com>
2025-07-25 06:14:06 -0700

    ssss

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,21 +79,19 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-  for (let i = 0; i < filterBtn.length; i++) {
 
-  filterBtn[i].addEventListener("click", function () {
+  for (let i = 0; i < filterItems.length; i++) {
 
-    let selectedValue = this.innerText.toLowerCase();
-    
-    filterFunc(selectedValue);
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
 
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
+  }
 
-  });
-
-}
 }
 
 // add event in all filter button items for large screen
